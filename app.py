33fe753c7d123cfa60278cfbde8fb501ea8e4560
akshay <akshay22.11.1995@gmail.com>
2022-06-12 13:10:04 +0530

    change the output

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return"Starting with ML"
+    return "I have made these changes in the app.py"
 
 if __name__=="__main__":
     app.run(debug=True)
